64e8958bce5f0166b19f7c6a38466b43dcc67fcf
Ben <benbruscella@gmail.com>
2011-06-14 23:29:59 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoAPI::Application.config.secret_token = '4a02f12190a48eddb03b84b44b9b63480cd71502c1dd837eb292fb36d3e4aeb4c52a2e6c1228b4b2cce426a9bd79245278b3a9d286cca2c4bb091a939faf1617'
