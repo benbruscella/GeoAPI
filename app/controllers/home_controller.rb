5835258e5c04301c0a9e370b22fe02541d9270b4
Ben <benbruscella@gmail.com>
2011-06-29 01:02:10 +1000

    using geocoder

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     if (params[:lat] && params[:lng])
-      render :json => Geokit::Geocoders::GoogleGeocoder.reverse_geocode("#{params[:lat]}, #{params[:lng]}") 
+      render :json => Geocoder.search("#{params[:lat]}, #{params[:lng]}") 
     end
     if (params[:address])
       render :json => Geocoder.search(params[:address]) 
